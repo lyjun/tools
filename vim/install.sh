daa3fd907697f3ef447b81cbea791a9d563ba5d2
yijyun <boy921@gmail.com>
2018-12-21 14:29:17 +0800

    check folders before install plugins

--- vim/install.sh
+++ vim/install.sh
@@ -3,8 +3,7 @@
 echo "install packages"
 sudo apt-get install -y vim git python-dev cmake clang
 
-echo "git clone VundleVim"
-[ -d ~/.vim/bundle/Vundle.vim ] || git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+[ -d ~/.vim/bundle/Vundle.vim ] || (echo "git clone VundleVim" && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim)
 
 echo "copy vimrc file"
 cp vimrc ~/.vimrc
@@ -12,5 +11,4 @@ cp vimrc ~/.vimrc
 echo "Plugin install"
 vim -c "PluginInstall" -c "qa"
 
-echo "Build YouCompleteMe"
-cd ~/.vim/bundle/YouCompleteMe && ./install.py --clang-completer
+[ -d ~/.vim/bundle/YouCompleteMe ] && (echo "Build YouCompleteMe" && cd ~/.vim/bundle/YouCompleteMe && ./install.py --clang-completer)
