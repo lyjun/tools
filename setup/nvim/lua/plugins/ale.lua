ef8aa0506588224b54c65478651ff176d399d1fa
Yi-Jyun Lin <boy921@gmail.com>
2024-12-22 18:33:57 +0800

    feat: linter checking

---
+++ setup/nvim/lua/plugins/ale.lua
@@ -0,0 +1,28 @@
+return {
+    --[[
+    ALE (Asynchronous Lint Engine) is a plugin providing linting (syntax checking and semantic errors)
+    in NeoVim 0.6.0+ and Vim 8.0+ while you edit your text files, and acts as a Vim Language Server Protocol client.
+
+    Tip:
+    1. Run the following to see what is currently configured:
+        :ALEInfo
+    2. you can install Language Server Protocol linters via Mason with linter tab.
+
+    --]]
+    "dense-analysis/ale",
+    config = function()
+        --[[
+        -- example for configuring ale
+
+        -- Configuration goes here.
+        local g = vim.g
+
+        g.ale_ruby_rubocop_auto_correct_all = 1
+
+        g.ale_linters = {
+            ruby = {'rubocop', 'ruby'},
+            lua = {'lua_language_server'}
+        }
+        --]]
+    end
+}
