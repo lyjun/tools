4f2de7e71dcfbd72328af695c908e8ffce173e08
YiJyun Lin <boy921@gmail.com>
2026-03-30 23:10:59 +0800

    refactor(nvim): update treesitter to new setup API
    Changes:
    - Migrate from nvim-treesitter.configs to nvim-treesitter module
    - Remove deprecated highlight config (now built-in)
    - Remove redundant comments
    - Set lazy = false for eager loading

--- setup/nvim/lua/linux/plugins/nvim-treesitter.lua
+++ setup/nvim/lua/linux/plugins/nvim-treesitter.lua
@@ -2,11 +2,12 @@ return {
     --[[
     don't forget to execute "TSUpdate" or install launquage with "TSInstall ${lang}"
     --]]
+    -- enabled = false,
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
     build = ':TSUpdate',
     config = function()
-        require("nvim-treesitter.configs").setup({
-            -- A list of parser names, or "all" (the five listed parsers should always be installed)
+        require("nvim-treesitter").setup({
             ensure_installed = {
                 "bash",
                 "c", "cmake", "comment", "cpp",
@@ -25,20 +26,9 @@ return {
                 "xml",
                 "yaml",
             },
-
-            -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
             auto_install = true,
-            -- List of parsers to ignore installing (or "all")
             ignore_install = {},
-            ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-            -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false
-            },
         })
     end,
 }
